39e7449042f7dac5f77d950083b0d94363cb8b8f
AlexandreLetscher <137295046+AlexandreLetscher@users.noreply.github.com>
2023-07-04 15:37:59 +0200

    sauvegarde

--- Algo_java/exemple/src/exercice2_1/App.java
+++ Algo_java/exemple/src/exercice2_1/App.java
@@ -2,6 +2,34 @@ package exercice2_1;
 
 import java.util.Scanner;
 
+/*
+ * 		VARIABLES
+
+		age est un entier 
+
+Debut du programme 
+    			"ecrire votre age"
+    			Lire <-- age 
+		Debut Si 
+    	Si
+   			a <1
+
+		Alors
+    		Ecrire " vous etes pas encore nee"
+		Sinon si 
+    		a<18
+		Alors
+    		ecrire "vous etes mineur"
+
+		Sinon 
+    		Ecrire "vous etes majeur"
+
+		Fin si 
+
+fin du programme 
+
+ */
+
 public class App {
 
 	public static void main(String[] args) {
