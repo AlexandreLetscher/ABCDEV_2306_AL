54733baba8a27b9bb8d2897c502b3c10a94f33de
AlexandreLetscher <137295046+AlexandreLetscher@users.noreply.github.com>
2023-07-27 12:28:04 +0200

    exercice tableau

---
+++ Algo_java/exemple/src/Exercice_fibonacci/App.java
@@ -0,0 +1,42 @@
+package Exercice_fibonacci;
+
+import java.util.Scanner;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		int position=2,nb,nbprecedent=0,nbcourant=1,nbsuivant = 0;
+	String resultat ="\t0\n\t1";
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Combien de nombres a la suite de Fibonacci souhaitez-vous affiché :");
+		nb=sc.nextInt();
+		
+		if(nb >2)
+		{
+			while (position< nb)
+			{
+				nbsuivant = nbprecedent + nbcourant;
+				resultat = resultat+"\n\t" + nbsuivant;
+				nbprecedent = nbcourant;
+				nbcourant = nbsuivant;
+				position = position +1;	
+			}
+		}
+		else 
+		{
+			nb=2;
+		}
+		
+		System.out.print("les "+nb+" premiers nombre de la suite de Fibonacci sont : \n" + resultat + " \n " + nb +" nombres affiches");
+		
+		
+			
+		
+		sc.close();
+	}
+
+}
