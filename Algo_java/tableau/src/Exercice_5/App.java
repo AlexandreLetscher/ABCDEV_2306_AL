496ddfba1e141c1523e0231cf0f04773a6948b0b
AlexandreLetscher <137295046+AlexandreLetscher@users.noreply.github.com>
2023-07-31 14:09:11 +0200

    exercice 5

--- Algo_java/tableau/src/Exercice_5/App.java
+++ Algo_java/tableau/src/Exercice_5/App.java
@@ -3,47 +3,28 @@ package Exercice_5;
 public class App {
 
 	public static void main(String[] args) {
-			int [] tab1 = new int [8];
-			tab1[0] = 4;
-			tab1[1] = 8;
-			tab1[2] = 7;
-			tab1[3] = 9;
-			tab1[4] = 1;
-			tab1[5] = 5;
-			tab1[6] = 4;
-			tab1[7] = 6;
-			
+			int [] tab1 = {4,8,7,9,1,5,4,6};
 			for(int i = 0; i < tab1.length; i++)
 			{
-				System.out.print(tab1[i]+" ");
+				System.out.print(tab1[i]+"  ");
 			}
 			
 			System.out.println(" ");
 			
-			int [] tab2 = new int [8];
-			tab2[0] = 7;
-			tab2[1] = 6;
-			tab2[2] = 5;
-			tab2[3] = 2;
-			tab2[4] = 1;
-			tab2[5] = 3;
-			tab2[6] = 7;
-			tab2[7] = 4;
-			
+			int [] tab2 = {7,6,5,2,1,3,7,4};
 			for(int i = 0; i < tab2.length; i++)
 			{
-				System.out.print(tab2[i]+" ");
+				System.out.print(tab2[i]+"  ");
 			}
 			
 			
-			System.out.println(" ");
+			System.out.println("   ");
 			int [] tab3 = new int [8];
-			
-		for (int i = 0; i < tab3.length; i++)
-		{
+			for (int i = 0; i < tab3.length; i++)
+			{
 			tab3[i] = tab1[i] + tab2[i];
-			System.out.print(tab3[i] + " ");
-		}
+			System.out.print(tab3[i] + "-");
+			}
 			
 	}
 
---
+++ Algo_java/tableau/src/Exercice_6/App.java
@@ -0,0 +1,31 @@
+package Exercice_6;
+
+public class App {
+
+	
+
+	public static void main(String[] args) {
+		
+		int tab1[] = {4,8,7,12};
+		int tab2[] = {3,6};
+		
+		int temp, signe = 0, resultat = 0;
+		int j;
+		
+		
+		for (int i=0; i < tab1.length; i++){
+		for ( j=0; j< tab2.length; j++);
+		String signeAffiche = (signe < (tab1.length * tab2.length)-1) ? " + " : " = ";  
+		signe++;
+		System.out.print(signeAffiche);
+		temp = tab2[j]*tab1[i];
+			
+			
+		
+			
+		}
+	}
+}
+
+	
+
