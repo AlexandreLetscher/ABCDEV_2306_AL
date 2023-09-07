76faa43865384560c61ee1f2e7c91cba1f60ceed
LETSCHER Alexandre <ALetscher@arfp.asso.fr>
2023-09-07 08:11:19 +0200

    sauvegarde

---
+++ Algo_java/oriente_objet/src/exercice_porte_de_garage/App.java
@@ -0,0 +1,20 @@
+package exercice_porte_de_garage;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Garage maPorte1 = new Garage("Aludoor", true, false, 25, 100, 0);
+		Garage maPorte2 = new Garage("Bricot depot", false, false, 0, 100, 0);
+		
+		boolean test1 = maPorte1.ouvrir();
+		boolean test2 = maPorte2.ouvrir();
+		boolean test3 = maPorte1.fermer();
+		boolean test4 = maPorte2.fermer();
+		boolean test5 = maPorte1.ouvrirPartiellement(65);
+		boolean test6 = maPorte2.ouvrirPartiellement(50);
+		boolean test7 = maPorte1.fermerPartiellement(50);
+		boolean test8 = maPorte2.fermerPartiellement(60);
+	}	
+
+}
---
+++ Algo_java/oriente_objet/src/exercice_porte_de_garage/Garage.java
@@ -0,0 +1,104 @@
+package exercice_porte_de_garage;
+
+public class Garage {
+private String marque = " ";
+private boolean estOuverte;
+private boolean estVerouiller;
+private int degresOuverture;
+private int degresMax = 100;
+private int degresMin = 0;
+
+
+	public Garage( String _marque, boolean _estOuverte, boolean _estVerouiller, int _degresOuverture, int _degresMax, int _degresMin)
+	{
+		this.marque = _marque;
+		this.estOuverte = _estOuverte;
+		this.estVerouiller = _estVerouiller;
+		this.degresOuverture = _degresOuverture;
+		this.degresMax = _degresMax;
+		this.degresMin = _degresMin;
+	}
+		public boolean ouvrir()
+		{
+			if(!this.estVerouiller && !this.estOuverte)
+			{
+				this.estOuverte = true;
+						return true; 
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+			public boolean fermer()
+			{
+				if(!this.estVerouiller && this.estOuverte)
+				{
+					this.estOuverte = false;
+						return true;
+				}
+					else
+				{
+					return false;
+				}
+			}
+			
+			public boolean ouvrirPartiellement(int degresOuvrable)
+			{
+				if(!this.estVerouiller && !this.estOuverte)
+				{
+					if (degresOuvrable >= 0)
+					{
+						if (this.degresOuverture + degresOuvrable < degresMax)
+						{
+							this.degresOuverture += degresOuvrable;
+							this.estOuverte=true;
+						return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}else {
+					return false;
+				}
+
+		}
+		
+			public boolean fermerPartiellement( int degresFermable)
+			{
+				if (this.estOuverte)
+				{
+					if (degresFermable <= degresMax)
+					{
+						if(this.degresOuverture - degresFermable > degresMin)
+							
+						{
+							this.degresOuverture -= degresFermable;
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}	
+					else
+					{
+						return false;
+					}
+
+				}
+				else
+				{
+					return false;
+				}
+			}
+		
+}
+
